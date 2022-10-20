b37e488a1cc4123bec51aaa769314deac3985324
Ethan <ethanchang34@yahoo.com>
2022-10-20 00:29:40 -0400

    added spotify path

--- src/mainRouter.ts
+++ src/mainRouter.ts
@@ -1,17 +1,18 @@
-import { ApiRouter } from "./api/apirouter"
+import { ApiRouter } from "./api/apirouter";
 import { SecRouter } from "./security/secrouter";
-import { Router } from "./shared/router"
+import { Router } from "./shared/router";
+import { SpotifyRouter } from "./spotify/spotifyrouter";
 
 //a derived router to be attached to the root of the site.
-export class MainRouter extends Router{
-	//attaches sub-routers off the root which internally will have there own routing.  This is to support modularization of the code.
-	public createRouter(): void {
-		//add routers
-		this.addRouter("/api",new ApiRouter());
-		this.addRouter("/security",new SecRouter());
-		
-		//add paths to this router if you need to add them directly
-		//this.router.get("/hello",(req,res)=>res.send('hello'));
-	}
+export class MainRouter extends Router {
+  //attaches sub-routers off the root which internally will have there own routing.  This is to support modularization of the code.
+  public createRouter(): void {
+    //add routers
+    this.addRouter("/api", new ApiRouter());
+    this.addRouter("/security", new SecRouter());
+    this.addRouter("/spotify", new SpotifyRouter());
 
+    //add paths to this router if you need to add them directly
+    //this.router.get("/hello",(req,res)=>res.send('hello'));
+  }
 }
