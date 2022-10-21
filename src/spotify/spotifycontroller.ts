a0d6bc46d4a22429dfe5334cff7b0d0338883f91
Ethan <ethanchang34@yahoo.com>
2022-10-21 14:35:15 -0400

    merge

--- src/spotify/spotifycontroller.ts
+++ src/spotify/spotifycontroller.ts
@@ -16,7 +16,7 @@ function makeid(length: number) {
 }
 
 const AUTH_URL: string = `https://accounts.spotify.com/authorize?client_id=3ecc3a4b5b974d02a9b9e12b7f2ace9b
-  &response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email
+  &response_type=code&redirect_uri=http://localhost:3000/api/hello&scope=streaming%20user-read-email
   %20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state`;
 
 export class SpotifyController {
@@ -41,38 +41,23 @@ export class SpotifyController {
     let state = makeid(16);
     let scope = `streaming user-read-email user-read-private user-library-read user-library-modify
     user-read-playback-state user-modify-playback-state`;
-    let client_id = process.env.spotifyClientID
-      ? process.env.spotifyClientID
-      : "";
-    let redirect_uri = process.env.spotifyRedirectURI
-      ? process.env.spotifyRedirectURI
-      : "";
-    let authorize_url =
-      "https://accounts.spotify.com/authorize?" +
-      querystring.stringify({
-        response_type: "code",
-        client_id: client_id,
-        scope: scope,
-        redirect_uri: redirect_uri,
-        state: state,
-      });
 
     // res.send({ status: "ok", result: authorize_url });
 
     res.redirect(
-      authorize_url
-      // "https://accounts.spotify.com/authorize?" +
-      //   querystring.stringify({
-      //     response_type: "code",
-      //     client_id: process.env.spotifyClientID
-      //       ? process.env.spotifyClientID
-      //       : "",
-      //     scope: scope,
-      //     redirect_uri: process.env.spotifyRedirectURI
-      //       ? process.env.spotifyRedirectURI
-      //       : "",
-      //     state: state,
-      //   })
+      "https://accounts.spotify.com/authorize?" +
+        querystring.stringify({
+          response_type: "code",
+          client_id: process.env.spotifyClientID
+            ? process.env.spotifyClientID
+            : "",
+          scope: scope,
+          redirect_uri: process.env.spotifyRedirectURI
+            ? process.env.spotifyRedirectURI
+            : "",
+          state: state,
+          show_dialog: true, //false(by default). Whether or not to force the user to approve the app again if they’ve already done so
+        })
     );
   }
 }
